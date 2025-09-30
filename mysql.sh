d3d77e730e2b0dec38f0aa475cf85afd08d110c7
sathishdevops38 <sathishdevops38@gmail.com>
2025-09-30 11:51:55 +0530

    mysql service

---
+++ mysql.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USER_ID=$(id -u)
+Logs_Folder="/var/log/shell-roboshop"
+Script_Name=$(echo $0 | cut -d "." -f1 )
+Logs_File="$Logs_Folder/$Script_Name.log"
+START_TIME=$(date +%s)
+SCRIPT_DIR=$PWD
+
+if [ $USER_ID -ne 0 ]; then
+    echo -e "$R ERRROR$N:: Run the script with root privillages"
+    exit 1
+fi
+
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 --- $R Failure $N"
+        exit 1
+    else
+        echo -e "$2--- $G Success $N"
+    fi
+}
+
+
+
+dnf install mysql-server -y
+validate $? "Insatlling mysql-server"
+
+
+systemctl enable mysqld
+validate $? "Enabeling  mysql"
+
+systemctl start mysqld  
+validate $? "Starting mysql"
+
+mysql_secure_installation --set-root-pass RoboShop@1
+validate $? "Password is set"
